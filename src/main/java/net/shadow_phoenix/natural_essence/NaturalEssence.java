a855773d13e45e520566faa47a83109ac8b91813
ShadowPhoenix220 <shad0w.ph0enix.399@gmail.com>
2024-09-17 11:57:21 -0400

    test

---
+++ src/main/java/net/shadow_phoenix/natural_essence/NaturalEssence.java
@@ -0,0 +1,16 @@
+package net.shadow_phoenix.natural_essence;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NaturalEssence implements ModInitializer {
+	public static final String MOD_ID = "natural_essence";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/shadow_phoenix/natural_essence/NaturalEssenceClient.java
@@ -0,0 +1,10 @@
+package net.shadow_phoenix.natural_essence;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NaturalEssenceClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
